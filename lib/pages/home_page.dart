5e22a097e310375df2de261b9de7f2a946eb2a51
LINDEX 171 <lindex171@gmail.com>
2024-05-06 19:22:36 +0000

    featured games background

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gamify_app/data.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,11 +9,34 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  var _deviceHeight;
+  var _deviceWidth;
+
   @override
   Widget build(BuildContext context) {
+    _deviceHeight = MediaQuery.of(context).size.height;
+    _deviceWidth = MediaQuery.of(context).size.width;
     return Scaffold(
-      body: Container(),
+      body: Stack(children: <Widget>[_featuredGamesWidget()]),
+    );
+  }
 
+  Widget _featuredGamesWidget() {
+    return SizedBox(
+      height: _deviceHeight * 0.50,
+      width: _deviceWidth,
+      child: PageView(
+        scrollDirection: Axis.horizontal,
+        children: featuredGames.map((_game){
+          return Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                fit: BoxFit.cover,
+                  image: NetworkImage(_game.coverImage.url))
+            ),
+          );
+        }).toList(),
+      ),
     );
   }
 }
