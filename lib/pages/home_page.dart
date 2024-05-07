43807f53671fc7a975edc3cca93a84a0d1743721
LINDEX 171 <lindex171@gmail.com>
2024-05-07 16:19:00 +0000

    Gradient

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -17,7 +17,10 @@ class _HomePageState extends State<HomePage> {
     _deviceHeight = MediaQuery.of(context).size.height;
     _deviceWidth = MediaQuery.of(context).size.width;
     return Scaffold(
-      body: Stack(children: <Widget>[_featuredGamesWidget()]),
+      body: Stack(children: <Widget>[
+        _featuredGamesWidget(),
+        _gradientBoxWidget(),
+      ]),
     );
   }
 
@@ -27,16 +30,34 @@ class _HomePageState extends State<HomePage> {
       width: _deviceWidth,
       child: PageView(
         scrollDirection: Axis.horizontal,
-        children: featuredGames.map((_game){
+        children: featuredGames.map((_game) {
           return Container(
             decoration: BoxDecoration(
-              image: DecorationImage(
-                fit: BoxFit.cover,
-                  image: NetworkImage(_game.coverImage.url))
-            ),
+                image: DecorationImage(
+                    fit: BoxFit.cover,
+                    image: NetworkImage(_game.coverImage.url))),
           );
         }).toList(),
       ),
+    );
+  }
+
+  Widget _gradientBoxWidget() {
+    return Align(
+        alignment: Alignment.bottomCenter,
+        child: Container(
+          height: _deviceHeight * 0.80,
+          width: _deviceWidth,
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+                colors: [
+                  Color.fromRGBO(35, 45, 59, 1.0),
+                  Colors.transparent,
+                ], stops: [0.65, 1.0],
+                begin: Alignment.bottomCenter,
+                end: Alignment.topCenter),
+          ),
+        ),
     );
   }
 }
