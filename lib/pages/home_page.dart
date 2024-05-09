621f23118a7b4b431bfdf161f4cab18d6014e449
LINDEX 171 <lindex171@gmail.com>
2024-05-09 15:22:55 +0000

    topBarWidget

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -20,6 +20,7 @@ class _HomePageState extends State<HomePage> {
       body: Stack(children: <Widget>[
         _featuredGamesWidget(),
         _gradientBoxWidget(),
+        _topLayerWidget(),
       ]),
     );
   }
@@ -44,20 +45,67 @@ class _HomePageState extends State<HomePage> {
 
   Widget _gradientBoxWidget() {
     return Align(
-        alignment: Alignment.bottomCenter,
-        child: Container(
-          height: _deviceHeight * 0.80,
-          width: _deviceWidth,
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-                colors: [
-                  Color.fromRGBO(35, 45, 59, 1.0),
-                  Colors.transparent,
-                ], stops: [0.65, 1.0],
-                begin: Alignment.bottomCenter,
-                end: Alignment.topCenter),
-          ),
+      alignment: Alignment.bottomCenter,
+      child: Container(
+        height: _deviceHeight * 0.80,
+        width: _deviceWidth,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(colors: [
+            Color.fromRGBO(35, 45, 59, 1.0),
+            Colors.transparent,
+          ], stops: [
+            0.65,
+            1.0
+          ], begin: Alignment.bottomCenter, end: Alignment.topCenter),
         ),
+      ),
+    );
+  }
+
+  Widget _topLayerWidget() {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: _deviceWidth * 0.05 ,vertical: _deviceHeight * 0.005 ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        mainAxisSize: MainAxisSize.max,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          _topBarWidget(),
+        ],
+      ),
+    );
+  }
+
+  Widget _topBarWidget() {
+    return SizedBox(
+      height: _deviceHeight * 0.13,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        mainAxisSize: MainAxisSize.max,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Icon(
+            Icons.menu,
+            color: Colors.white,
+            size: 30,
+          ),
+          Row(
+            children: <Widget>[
+              Icon(
+                Icons.search,
+                color: Colors.white,
+                size: 30,
+              ),
+              SizedBox(width: _deviceWidth * 0.03,),
+              Icon(
+                Icons.notifications_none,
+                color: Colors.white,
+                size: 30,
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
